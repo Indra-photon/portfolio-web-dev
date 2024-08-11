d77dd949efb03ba039f0359c7285febb183d3d16
Indranil Maiti <indranilmaiti16@gmail.com>
2024-08-11 20:55:03 +0200

    security updated

---
+++ src/components/DesktopDesign/DesktopDesign.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const DesktopDesign = () => {
+  return (
+        <div>
+            <div class="outer-box">
+        
+                <div class = "side-container1">
+                    <section class="side-container1-section-top">
+                        <div class="photon small-circle1">  </div>
+                        <div class="photon small-circle2"> </div>
+                        <div class="photon small-circle3"> </div>
+                    </section>
+                    <section class="side-container1-section-bottom">
+                        
+                        <hr class="blank line1">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line1">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line1">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                        <hr class="blank line2">
+                    </section>
+        
+                </div>
+                <div class="side-container2">
+                    <section class="img">
+                        <img src="svg.svg" alt="Computer" height="300px" width="300px">
+                    </section>
+                    <section class="text">
+                    <h1> <span class="span1">We're Hiring</span> <p> <span class="span2">WEB</span> <br><span class="span3">DESIGNER
+                    </span><p> <span class="span4">Apply Now</span></p></h1>
+                    </section>
+                </div>
+            </div>
+            <hr class="short-stand"></hr>
+            <hr class="long-stand"></hr>
+        
+        </div>
+  )
+}
+
+export default DesktopDesign
--- src/components/Form/ContactEmailjs.jsx
+++ src/components/Form/ContactEmailjs.jsx
@@ -1,10 +1,10 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
+const YOUR_SERVICE_ID = process.env.YOUR_SERVICE_ID;
+const YOUR_TEMPLATE_ID = process.env.YOUR_TEMPLATE_ID;
+const YOUR_PUBLIC_ID = process.env.YOUR_PUBLIC_ID;
 
-const YOUR_SERVICE_ID = 'service_lsdq26v';
-const YOUR_TEMPLATE_ID = 'template_qtikgih';
-const YOUR_PUBLIC_ID = 'tQy-Zlw9w6LiBRChZ';
 
 export const ContactUs = () => {
   const form = useRef();
