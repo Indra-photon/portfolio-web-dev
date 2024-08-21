a5792e6dafb104b78df3681768713b6f2f848533
Indranil Maiti <indranilmaiti16@gmail.com>
2024-08-21 10:24:47 +0200

    responsivenss added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,3 +1,4 @@
+/*Old code*/
 import { useState } from 'react'
 import '../components/Homepage.css'
 import pic1 from '../assets/pic1_homepage.svg'
@@ -13,7 +14,7 @@ const text = "Welcome to WebWorks Gallery".split(" ");
 
 const Homepage = () => {
 
-    const [isTextComplete, setIsTextComplete] = useState(false);
+    const [isTextComplete, setIsTextComplete] = useState(true);
 
     const handleTypewriterComplete = () => {
         setIsTextComplete(true);
@@ -23,119 +24,112 @@ const Homepage = () => {
     <div>
     <Navbar />
     <div className = 'h-max bg-amber-200'>
-        <div className = 'h-[550px] bg-amber-200 flex px-10 py-10'>
-            
+        <div className = 'h-auto bg-amber-200 flex py-10'>
             <section>
-                <section className = 'text-6xl font-serif flex items-center justify-center'>
-                
-                <div>
-                {text.map((el, i) => (
-                    <motion.span
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{
-                            duration: 0.25,
-                            delay: i / 5
-                        }}
-                        key={i}
-                        onAnimationComplete={i === text.length - 1 ? handleTypewriterComplete : undefined}
-                        className = 'm-2'
-                    >
-                        {el}{}
-                    </motion.span>
-                ))}
-                </div>
-                </section>
-                <div className = {`text-3xl font-serif flex gap-10 mt-10 items-center ${isTextComplete ? 'opacity-100 transition-all duration-500' : 'opacity-0'}`}>
+                <div className = {`text-3xl font-serif flex gap-10 items-center ${isTextComplete ? 'opacity-100 transition-all duration-500' : 'opacity-0'}`}>
                     <section>
-                        <img src = {pic1}/>
-                    </section>
-                    <section>
-                        <section>
-                            <p className = 'pt-10'> With a blend of creativity and technical expertise, I bring ideas to life through 
+                        <section className = 'md:px-5 sm:px-5'>
+                            <p className = 'pt-1 sm:w-screen sm:px-5 sm:pl-5'> With a blend of creativity and technical expertise, I bring ideas to life through 
                             elegant <span className = 'text-orange-800 font-bold'>front end designs</span> and robust <span className = 'text-orange-800 font-bold'>
                             back-end solutions</span></p>
                         </section>
                         <section className='flex justify-center mt-5 mx-10'>
                             <div
-                                className={`w-64 h-fit bg-black rounded-2xl transition-all duration-1000 ease-linear flex justify-center items-center py-5 ${isTextComplete ? 'opacity-30 w-full' : 'opacity-0'}`}
+                                className={`
+                                    w-full md:w-3/4 text-orange-800 lg:w-2/3 bg-[rgb(245, 158, 11)] rounded-2xl transition-all duration-1000 ease-linear flex flex-col md:flex-row justify-center items-center py-5 ${isTextComplete ? 'opacity-100' : 'opacity-0'}`}
                             >
-                                <div className='flex gap-10'>
-                                    <section>
-                                    <div className = 'flex gap-2'>
-                                        <img src = 'https://sisyphusinfotech.com/assets/our-strength/cost.png' className = 'h-8 w-8' />
-                                        <p className='text-3xl text-white'>Best UI/UX Design</p>
-                                    </div>
-                                    <hr className='h-1 bg-slate-400 mb-5' />
-                                    <div className = 'flex gap-2'>
-                                        <img src = 'https://sisyphusinfotech.com/assets/our-strength/cost.png' className = 'h-8 w-8' />
-                                        <p className='text-3xl text-white'>Device friendly design</p>
-                                    </div>
-                                    <hr className='h-1 bg-slate-400 mb-5' />
+                                <div className='flex md:flex-row gap-20 md:gap-10 px-5 md:px-0 sm:flex-col sm:gap-2'>
+                                    <section className='flex flex-col gap-4 sm:flex-col'>
+                                        <div className='flex items-center gap-2'>
+                                            <img src='https://sisyphusinfotech.com/assets/our-strength/cost.png' className='h-8 w-8' alt='UI/UX Design' />
+                                            <p className='text-2xl md:text-2xl lg:text-3xl text-orange-800'>Best UI/UX Design</p>
+                                        </div>
+                                        <hr className='h-1 bg-slate-400' />
+                                        <div className='flex items-center gap-2'>
+                                            <img src='https://sisyphusinfotech.com/assets/our-strength/cost.png' className='h-8 w-8' alt='Device Friendly Design' />
+                                            <p className='text-2xl md:text-2xl lg:text-3xl text-orange-800'>Device friendly design</p>
+                                        </div>
+                                        <hr className='h-1 bg-slate-400' />
                                     </section>
-                                    <section>
-                                    <div className = 'flex gap-2'>
-                                        <img src = 'https://sisyphusinfotech.com/assets/our-strength/service.png' className = 'h-8 w-8' />
-                                        <p className='text-3xl text-white'>Service Oriented</p>
-                                    </div>
-                                    <hr className='h-1 bg-slate-400 mb-5' />
-                                    <div className = 'flex gap-2'>
-                                        <img src = 'https://sisyphusinfotech.com/assets/our-strength/detail.png' className = 'h-8 w-8' />
-                                        <p className='text-3xl text-white'>Detail Oriented</p>
-                                    </div>
-                                    <hr className='h-1 bg-slate-400 mb-5' />
+                                    <section className='flex flex-col gap-4 sm:flex-col'>
+                                        <div className='flex items-center gap-2'>
+                                            <img src='https://sisyphusinfotech.com/assets/our-strength/service.png' className='h-8 w-8' alt='Service Oriented' />
+                                            <p className='text-2xl md:text-2xl lg:text-3xl text-orange-800'>Service Oriented</p>
+                                        </div>
+                                        <hr className='h-1 bg-slate-400' />
+                                        <div className='flex items-center gap-2'>
+                                            <img src='https://sisyphusinfotech.com/assets/our-strength/detail.png' className='h-8 w-8' alt='Detail Oriented' />
+                                            <p className='text-2xl md:text-2xl lg:text-3xl text-orange-800'>Detail Oriented</p>
+                                        </div>
+                                        <hr className='h-1 bg-slate-400' />
                                     </section>
                                 </div>
                             </div>
                         </section>
                     </section>
+                </div>  
+            </section>      
+        </div>
+
+        <section>
+            <section className = 'py-0 px-48 bg-amber-200 md:px-10 sm:px-0'>
+                <div className = 'flex justify-center text-4xl'>
+                    <p className = 'pb-4'> What I can do </p>
                 </div>
-                
-                    
+                <section className = 'md:flex md:justify-center'>
+                    <HoverCard index={0}>
+                        <div className = 'flex gap-5 pb-8 md:flex md:justify-center sm:flex-col sm:w-screen'>
+                            <section className = 'sm:flex sm:justify-center'>
+                                <img src={pic2} className = 'w-28 h-28'></img>
+                            </section>
+                            <section>
+                                <div className = 'sm:flex sm:justify-center'>
+                                <p className = 'text-orange-800 text-3xl pb-5'>Design what you want</p>
+                                </div>
+                                <div className = 'sm:flex sm:justify-center sm:px-5'>
+                                <p className = 'text-xl md:text-2xl'>I like to keep it simple. My goals are to focus on typography, content and conveying the message that you want to send.</p>
+                                </div>
+                            </section>           
+                        </div>
+                    </HoverCard>
+                </section>
+                <section className = 'md:flex md:justify-center'>
+                    <HoverCard index={1}>
+                        <div className = 'flex gap-5 sm:flex-col-reverse'>
+                            <section>
+                                <div className = 'sm:flex sm:justify-center'>
+                                <p className = 'text-orange-800 text-3xl pb-5'>Develop what you need</p>
+                                </div>
+                                <div className = 'sm:flex sm:justify-center sm:px-5'>
+                                <p className = 'text-xl md:text-2xl'>I am a developer, so I know how to create your website to run across devices using the latest technologies available.</p>
+                                </div>
+                            </section>
+                            <section className = 'sm:flex sm:justify-center'>
+                                <img src={pic3} className = 'w-28 h-28'></img>
+                            </section>         
+                        </div>
+                    </HoverCard>
+                </section>
             </section>
 
-                   
-        </div>
-        <section className = 'py-0 px-48 bg-amber-200'>
-            <div className = 'flex justify-center text-4xl'>
-                <p className = 'pb-4'> What I can do </p>
-            </div>
-            <HoverCard index={0}>
-                <div className = 'flex gap-5 pb-8'>
-                    <img src={pic2} className = 'w-28 h-28'></img>
-                    <section>
-                        <p className = 'text-orange-800 text-3xl pb-5'>Design what you want</p>
-                        <p className = 'text-xl'>I like to keep it simple. My goals are to focus on typography, content and conveying the message that you want to send.</p>
-                    </section>           
+            <section className = 'py-10 px-48 bg-amber-200 md:px-10 sm:px-0'>
+                <div className = 'flex justify-center text-4xl'>
+                    <p className = 'pb-0'> I can help </p>
                 </div>
-            </HoverCard>
-            <HoverCard index={1}>
-                <div className = 'flex gap-5'>
-                    <section>
-                        <p className = 'text-orange-800 text-3xl pb-5'>Develop what you need</p>
-                        <p className = 'text-xl'>I am a developer, so I know how to create your website to run across devices using the latest technologies available.</p>
-                    </section>
-                    <img src={pic3} className = 'w-28 h-28'></img>          
-                </div>
-            </HoverCard>
-        </section>
-
-        <section className = 'py-10 px-48 bg-amber-200'>
-            <div className = 'flex justify-center text-4xl'>
-                <p className = 'pb-0'> I can help </p>
-            </div>
-
-            <HoverCard index={0}>
-            <div>
-                <section className='flex justify-center items-center'>
-                    <p className = 'text-orange-800 text-3xl pb-5'>I’m currently available for freelance work.</p>
+                <section className = 'md:flex md:justify-center'>
+                    <HoverCard index={0}>
+                    <div>
+                        <section className='flex justify-center items-center sm:flex sm:justify-center sm:px-5'>
+                            <p className = 'text-orange-800 text-3xl pb-5'>I’m currently available for freelance work.</p>
+                        </section>
+                        <section className='flex justify-center items-center sm:px-5'>
+                        <p className = 'text-xl md:text-2xl'>If you have a project that you want to get started, 
+                        think you need my help with something or just fancy saying hey, then get in touch.</p>
+                        </section>
+                    </div>
+                    </HoverCard>
                 </section>
-                <section className='flex justify-center items-center'>
-                <p className = 'text-xl'>If you have a project that you want to get started, 
-                think you need my help with something or just fancy saying hey, then get in touch.</p>
-                </section>
-            </div>
-            </HoverCard>
+            </section>
         </section>
     </div>
     <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,42 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      'sm': {'min': '0px', 'max': '800px'},
+      // => @media (min-width: 0px and max-width: 767px) { ... }
 
+      'md': {'min': '801px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        Roboto : ['Roboto'],
+        Montserrat : ["Montserrat", "sans-serif"]
+      },
+
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+
+      backgroundImage: {
+        'footer-texture': "url('./public/sunset.jpg')"
+      }
+    },
+  },
+
+  plugins: [
+    
+  ]
+  ,
+}
