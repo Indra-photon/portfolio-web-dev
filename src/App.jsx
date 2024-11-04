ca9bed37205e56d18e5c0537ad764916faf9b1e5
Indranil Maiti <indranilmaiti1@gmail.com>
2024-11-05 01:37:52 +0530

    portofolio updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-import About from './components/About'
-import HomepageTop from './components/HomepageTop'
 import { Route, Routes } from 'react-router-dom';
 import Portfolio from './components/Portfolio.jsx';
 
@@ -15,8 +12,6 @@ function App() {
           <Routes>
             <Route path = '/' element = {<Portfolio />} />
             <Route path = '/home' element = {<Portfolio />} />
-            <Route path='/homepage' element = {<Portfolio />}/>
-            <Route path='/about' element = {<About />}/>
           </Routes>
         </main>
         
